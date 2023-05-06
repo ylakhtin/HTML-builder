c94cca40ed6fd95b06e86e17ce55d6cdca0a611d
Yuriy Lakhtin <yuriy.lakhtin@gmail.com>
2023-05-06 16:24:01 +0200

    fix: use stream for task 1

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,14 +2,5 @@ const fs = require('fs');
 const path = require('path');
 const { stdout } = process;
 
-fs.readFile(
-  path.join(__dirname, 'text.txt'),
-  'utf-8',
-  (err, data) => {
-    if (err) {
-      throw err;
-    } else {
-      stdout.write(data);
-    }
-  }
-);
+const reading = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+reading.on('data', input => stdout.write(input));
